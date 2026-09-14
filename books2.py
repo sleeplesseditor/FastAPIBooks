35b198b14e1881b7e70a9001530f7c815e92366c
Josh <josh@bryson.me.uk>
2026-09-14 12:45:14 +0100

    Pydantic configuration updated

--- books2.py
+++ books2.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from typing import Optional
+from fastapi import FastAPI, Body
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
@@ -8,15 +10,34 @@ class Book:
     author: str
     description: str
     rating: int
-    year: int
+    published_date: int
+    rating: int
 
-    def __init__(self, id, title, author, description, rating, year):
+    def __init__(self, id, title, author, description, rating, published_date):
         self.id = id
         self.title = title
         self.author = author
         self.description = description
         self.rating = rating
-        self.year = year
+        self.published_date = published_date
+
+class BookRequest(BaseModel):
+    id: Optional[int] = Field(description="ID is not needed on create", default=None)
+    title: str = Field(min_length=3)
+    author: str = Field(min_length=1)
+    description: str = Field(min_length=1, max_length=100)
+    rating: int = Field(gt=0, lt=6)
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "title": "A new title",
+                "author": "codingwithroby",
+                "description": "A new description of a book",
+                "rating": 5,
+            }
+        }
+    }
 
 
 BOOKS = [
@@ -31,3 +52,12 @@ BOOKS = [
 @app.get("/books")
 async def read_all_books():
     return BOOKS
+
+@app.post("/create_book")
+async def create_book(book_request: BookRequest):
+    new_book = Book(**book_request.model_dump())
+    BOOKS.append(find_book_id(new_book))
+
+def find_book_id(book: Book):
+    book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1
+    return book
